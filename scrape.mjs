617aff82b08be43d47fceea81089fd56a0cb8c9d
kaverisonani <40573215+kaverisonani@users.noreply.github.com>
2025-03-14 11:20:25 -0400

    Update scrape.mjs

--- scrape.mjs
+++ scrape.mjs
@@ -3,10 +3,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 console.log('Launching Puppeteer...');
-const browser = await puppeteer.launch({
-	headless: true, // Make sure it's headless mode
-	args: ['--no-sandbox', '--disable-setuid-sandbox'] // Disable sandboxing
-});
+const browser = await puppeteer.launch({ headless: false });
 const page = (await browser.pages())[0];
 
 const feedURL = 'https://www.ubereats.com/feed?diningMode=PICKUP&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMjQ3OCUyMFJpbW9zYSUyMENydCUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmU2NTExNTk5LWYxMWEtY2Q3MC0xZTViLTFmNjA1Njg2YjdkNCUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBNDMuOTAyMzM0JTJDJTIybG9uZ2l0dWRlJTIyJTNBLTc4LjkwMzM2MyU3RA';
